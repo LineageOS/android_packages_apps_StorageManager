b26f457e30d6d444c873efaaf26181139bb92aed
Daniel Nishi <dhnishi@google.com>
2016-06-01 15:29:56 -0700

    Port photo/video clearing to the Storage Manager.
    This behavior previously existed in the Settings' Deletion
    Helper and is being ported to the new APK.
    
    Bug: 28965462
    Change-Id: I3974ea15d665b061d45caf4b3413a56cef1453d9

---
+++ src/com/android/storagemanager/deletionhelper/PhotosDeletionPreference.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.storagemanager.deletionhelper;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.text.format.Formatter;
+
+import com.android.storagemanager.R;
+
+/**
+ * Preference to handle the deletion of photos and videos in the Deletion Helper.
+ */
+public class PhotosDeletionPreference extends DeletionPreference {
+    public static final int DAYS_TO_KEEP = 30;
+
+    public PhotosDeletionPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        updatePreferenceText(0, 0);
+    }
+
+    /**
+     * Updates the title and summary of the preference with fresh information.
+     */
+    public void updatePreferenceText(int items, long bytes) {
+        Context context = getContext();
+        setTitle(context.getString(R.string.deletion_helper_photos_title, items));
+        setSummary(context.getString(R.string.deletion_helper_photos_summary,
+                Formatter.formatFileSize(context, bytes), DAYS_TO_KEEP));
+    }
+
+    @Override
+    public void onFreeableChanged(int items, long bytes) {
+        super.onFreeableChanged(items, bytes);
+        updatePreferenceText(items, bytes);
+    }
+}
