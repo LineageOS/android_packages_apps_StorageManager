2e95da3f969a294ce2978d05046dd758388aa0f1
Paul Duffin <paulduffin@google.com>
2017-01-17 14:49:42 +0000

    Prepare for removal of legacy-test from default targets
    In preparation for removing junit classes from the Android API
    the legacy-test target will be removed from the
    TARGET_DEFAULT_JAVA_LIBRARIES. This change adds explicit
    dependencies on junit and/or legacy-android-test to ensure that
    modules will compile properly once it is removed.
    
    Bug: 30188076
    Test: make checkbuild
    Change-Id: I84ed2bb7ea7c9cca241cc5ccac6ecf70521d8c88

--- tests/unit/Android.mk
+++ tests/unit/Android.mk
@@ -6,7 +6,10 @@ LOCAL_MODULE_TAGS := tests
 
 LOCAL_JAVA_LIBRARIES := android.test.runner
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-test mockito-target-minus-junit4
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-test \
+    mockito-target-minus-junit4 \
+    legacy-android-test
 
 # Include all test java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
